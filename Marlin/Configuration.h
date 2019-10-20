6be1097270499ed04512fc20cf2ec30adbf6f5f2
Pedro Martinez <pedromartinez11@gmail.com>
2019-10-19 21:23:06 -0700

    mute lcd feedback so that it doesn't interfere with bltouch probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1513,8 +1513,8 @@
 // Note: Test audio output with the G-Code:
 //  M300 S<frequency Hz> P<duration ms>
 //
-//#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 2
-//#define LCD_FEEDBACK_FREQUENCY_HZ 5000
+#define LCD_FEEDBACK_FREQUENCY_DURATION_MS 0
+#define LCD_FEEDBACK_FREQUENCY_HZ 0
 
 //=============================================================================
 //======================== LCD / Controller Selection =========================
